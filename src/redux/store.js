7099ed4bfb3d8150b66147056c926f2dbbb792c1
mafergus <matthew.n.ferguson@gmail.com>
2020-06-03 23:23:30 +0400

    Adding redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [];
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
+  middleware.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(...middleware),
+  )
+);
 
 export default store;
